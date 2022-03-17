2e535459a171b306df25554c3c8ae72e02cf97c2
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-03-17 22:55:40 +0100

    Styling search components

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -3,16 +3,16 @@ import { Col, Container, Form, Row } from "react-bootstrap";
 
 const Search = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
+    <Container className='mt-4'>
+      <Row className='justify-content-center'>
+        <Col xs ={12} md ={8} lg={6}>
           <Form>
             <Row>
-              <Col>
+              <Col xs={9}>
                 <Form.Control placeholder="Search for new images....." />
               </Col>
               <Col>
-                <button variant ="primary" type = "submit">Search</button>
+                <button variant="primary" type = "submit">Search</button>
               </Col>
             </Row>
           </Form>
