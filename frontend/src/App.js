2a343bcdc4250758c32825a974bd10c9623cc29a
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-03-18 15:38:22 +0100

    json setting modification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,9 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -18,7 +20,7 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       });
-      setWord("");
+    setWord("");
   };
   return (
     <div className="App">
